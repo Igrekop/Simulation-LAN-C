a64df0b148d1b272cb4add5c99b02f6409f40b81
salmane <salmane@unistra.fr>
2025-05-26 11:41:07 +0200

    switch structure

---
+++ include/switch.h
@@ -0,0 +1,15 @@
+#include <stdio.h>     
+#include <stdlib.h>   
+#include <string.h>    
+#include "mac_addresses.h"
+
+typedef struct {	
+   AdresseMac mac;
+   unsigned int NBports;
+   uint16_t BID;
+
+} switch;
+ 
+
+typedef struct {
+} 
