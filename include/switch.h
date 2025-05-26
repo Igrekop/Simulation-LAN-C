6360caf01a02f1aa55200f19b1d5a74115b68c74
Victor Sihr <vsihr@unistra.fr>
2025-05-26 12:13:54 +0200

    suppresion mac adresse et ip adresse

--- include/switch.h
+++ include/switch.h
@@ -1,10 +1,9 @@
 #include <stdio.h>     
 #include <stdlib.h>   
 #include <string.h>    
-#include "mac_addresses.h"
 
 typedef struct {	
-   AdresseMac mac;
+   
    unsigned int NBports;
    uint16_t BID;
 
