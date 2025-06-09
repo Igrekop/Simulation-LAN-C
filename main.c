2342fd07dfcbbc1e9483464d7afe49842ebb74a4
Valentin <valentin.petit2@etu.unistra.fr>
2025-06-09 03:05:59 +0200

    Partie 2

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "equipement.h"
 #include "reseau.h"
+#include <stdlib.h>
 
 int main() {
+
+    // Partie 1
     ReseauLocal r = {0};
     r.nb_equipements = 2;
 
@@ -23,6 +26,30 @@ int main() {
     r.matrice_adjacence[1][0] = 4;
 
     afficher_reseau(&r);
-    return 0;
+    //return 0; // --> en commentaire pour pouvoir continuer
     // Affichage du réseau
+    
+
+    // Partie 2
+    ReseauLocal mon_reseau; // Déclare une structure ReseauLocal
+    
+    // Initialisation de base au cas où le chargement échoue
+    mon_reseau.nb_equipements = 0;
+    // La matrice d'adjacence sera initialisée par charger_reseau_depuis_fichier
+
+    printf("Chargement du réseau depuis le fichier 'reseau_config.txt'...\n\n");
+    
+    // Appel de la fonction de chargement du réseau
+    if (charger_reseau_fichier("reseau_config.txt", &mon_reseau) != 0) {
+        fprintf(stderr, "Échec du chargement du réseau.\n");
+        return EXIT_FAILURE; // Quitte le programme avec un code d'erreur
+    }
+    
+    printf("Réseau chargé avec succès !\n\n");
+    
+    // Affichage du réseau chargé
+    afficher_reseau(&mon_reseau);
+    
+    return EXIT_SUCCESS;
+
 }
