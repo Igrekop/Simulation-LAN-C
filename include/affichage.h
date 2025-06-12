2a972b7bcdf5512b5404f6374f6c15781f153af9
Igrekop <yvann.du.soub@gmail.com>
2025-06-13 01:57:00 +0200

    j'ai du ajouter plein de truc juste changer les structures de place ça fait tout buguer

--- include/affichage.h
+++ include/affichage.h
@@ -3,7 +3,7 @@
 
 #include "equipement.h"
 
-void afficher_mac(AdresseMAC mac);
+void afficher_mac(mac_addr_t mac);
 void afficher_ip(ip_addr_t ip);
 void afficher_station(station_t s);
 void afficher_switch(switch_t sw);
